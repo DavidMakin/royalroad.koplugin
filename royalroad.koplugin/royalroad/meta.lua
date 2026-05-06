ac914042d563a09ab3257e35f3523dd301be5f33
David Makin <david.makin@agreena.com>
2026-05-06 16:55:21 +0100

    chore: bump version to v0.9.0

--- royalroad.koplugin/royalroad/meta.lua
+++ royalroad.koplugin/royalroad/meta.lua
@@ -1,5 +1,5 @@
 return {
-    version              = "0.8.1",
+    version              = "0.9.0",
     min_koreader_version = "0.8.1",
     description          = "Downloads stories from Royal Road as EPUB files.",
 }
